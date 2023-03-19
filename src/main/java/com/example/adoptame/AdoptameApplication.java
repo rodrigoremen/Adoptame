779a55f1df5521f5148da4640b0db9be6bef0dce
Alexqander <20203tn032@utez.edu.mx>
2023-03-18 20:17:23 -0600

    [CONFIG] Configuracion de tailwind css

---
+++ src/main/java/com/example/adoptame/AdoptameApplication.java
@@ -0,0 +1,13 @@
+package com.example.adoptame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adoptame/AdoptameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adoptame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
