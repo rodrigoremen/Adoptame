d30adb0417e2235bef51463130617915a5ca5ebc
Erik Tapia <20203tn152@utez.edu.mx>
2023-04-04 11:51:30 -0600

    se hacen cambios a los archivos de acuerdo con el proyecto referencia

--- src/main/java/com/example/adoptame/WebSecurityConfig.java
+++ src/main/java/com/example/adoptame/WebSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -19,8 +21,14 @@ public class WebSecurityConfig {
     @Autowired
     private DataSource dataSource;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public void globalConfigure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication(dataSource);
+        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?").passwordEncoder(passwordEncoder()).authoritiesByUsernameQuery("SELECT u.username, r.authority FROM authorities a INNER JOIN users u ON u.id_user = a.user_id INNER JOIN roles r ON r.id_rol = a.rol_id WHERE u.username = ?");
+        ;
     }
 
     @Bean
@@ -31,11 +39,11 @@ public class WebSecurityConfig {
         });
 
         http.formLogin((login) -> {
-            login.loginPage("/login").permitAll().defaultSuccessUrl("/index", true);
+            login.loginPage("/login").permitAll().defaultSuccessUrl("/index").failureUrl("/login-error");
 
         });
         http.logout((logout) -> {
-            logout.permitAll();
+            logout.logoutSuccessUrl("/login?logout").permitAll();
         });
         return http.build();
     }
